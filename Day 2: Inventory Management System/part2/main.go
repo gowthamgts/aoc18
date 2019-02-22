ede84d4bd6012a450081826d552cdfd97906f1d1
Gowtham Gopalakrishnan <gowthamgts12@gmail.com>
2019-02-22 14:35:39 +0530

    Moved to util class

--- Day 2: Inventory Management System/part2/main.go
+++ Day 2: Inventory Management System/part2/main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
+	"github.com/gowthamgts/aoc18"
 )
 
 func main() {
 
-	lines := readFromInputFile("../input.txt")
+	lines := util.ReadFromInputFile("../input.txt")
 
 	var matchingString string
 
@@ -22,29 +20,6 @@ func main() {
 	}
 
 	fmt.Println(matchingString)
-}
-
-// readFromInputFile reads from the file line by line
-// and returns the slice.
-func readFromInputFile(path string) []string {
-	file, err := os.Open(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	var lines []string
-
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-
-	return lines
 }
 
 // checkStringMatch checks the two strings and returns
---
+++ util.go
@@ -0,0 +1,30 @@
+package util
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+// readFromInputFile reads from the file line by line
+// and returns the slice.
+func ReadFromInputFile(path string) []string {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var lines []string
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return lines
+}
